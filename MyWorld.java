01c2663c5cfb446cc3d06e67ad7ac75ffbf4ed71
Keith-McClelland <keithmcclelland5@gmail.com>
2025-04-30 08:36:16 -0400

    Added Falling Pizza

--- MyWorld.java
+++ MyWorld.java
@@ -5,5 +5,7 @@ public class MyWorld extends World {
         super(600, 400, 1);
         Elephant elephant = new Elephant();
         addObject(elephant, 300, 300);
+        Pizza pizza = new Pizza();
+        addObject(pizza, 300, 0);
     }
 }
