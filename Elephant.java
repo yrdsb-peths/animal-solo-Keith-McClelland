df0c96dbe69853352fd0567e33a9ab082b4ad64c
Keith-McClelland <keithmcclelland5@gmail.com>
2025-05-07 08:37:30 -0400

    Added background and sounds

--- Elephant.java
+++ Elephant.java
@@ -8,10 +8,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Elephant extends Actor
 {
-    /**
-     * Act - do whatever the Elephant wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    GreenfootSound elephantSound = new GreenfootSound("elephantcub.mp3");
     public void act()
     {
         if(Greenfoot.isKeyDown("left")){
@@ -28,6 +25,7 @@ public class Elephant extends Actor
             MyWorld world = (MyWorld) getWorld();
             world.createPizza();
             world.increaseScore();
+            elephantSound.play();
         }
         
     }
