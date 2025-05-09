1a1771471976388f5d9569c910bcd0ded7d53f0e
Keith-McClelland <keithmcclelland5@gmail.com>
2025-05-09 08:50:14 -0400

    Added animations

--- Elephant.java
+++ Elephant.java
@@ -9,6 +9,20 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Elephant extends Actor
 {
     GreenfootSound elephantSound = new GreenfootSound("elephantcub.mp3");
+    GreenfootImage[] idle = new GreenfootImage[8];
+    
+    public Elephant(){
+        for(int i = 0; i <idle.length; i++){
+            idle[i] = new GreenfootImage("C:/Users/khew2/Pictures/elephant_idle/idle" + i + ".png");
+        }
+        setImage(idle[0]);
+    }
+    
+    int imageIndex = 0;
+    public void animateElephant(){
+        setImage(idle[imageIndex]);
+        imageIndex = (imageIndex + 1) % idle.length;
+    }
     public void act()
     {
         if(Greenfoot.isKeyDown("left")){
@@ -18,6 +32,7 @@ public class Elephant extends Actor
             move(2);
         }
         eatPizza();
+        animateElephant();
     }
     public void eatPizza(){
         if(isTouching(Pizza.class)){
