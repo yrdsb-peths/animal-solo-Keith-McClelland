2e3a61331cb84a753d26bb22101fef7e9b9400d6
Keith-McClelland <keithmcclelland5@gmail.com>
2025-05-12 09:02:29 -0400

    Added timer

--- Elephant.java
+++ Elephant.java
@@ -13,7 +13,7 @@ public class Elephant extends Actor
     GreenfootImage[] idleLeft = new GreenfootImage[8];
     
     String facing = "right";
-    
+    SimpleTimer animationTimer = new SimpleTimer();
     public Elephant(){
         for(int i = 0; i <idleRight.length; i++){
             idleRight[i] = new GreenfootImage("C:/Users/khew2/Pictures/elephant_idle/idle" + i + ".png");
@@ -24,11 +24,16 @@ public class Elephant extends Actor
             idleLeft[i].mirrorHorizontally();
             idleLeft[i].scale(100, 100);
         }
+        animationTimer.mark();
         setImage(idleRight[0]);
     }
     
     int imageIndex = 0;
     public void animateElephant(){
+        if(animationTimer.millisElapsed() < 100){
+            return;
+        }
+        animationTimer.mark();
         if(facing .equals("right")){
             setImage(idleRight[imageIndex]);
             imageIndex = (imageIndex + 1) % idleRight.length;
